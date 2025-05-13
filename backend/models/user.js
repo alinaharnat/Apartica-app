82eeaa971219665cb7d366794a08bfc501e5aab1
dmulya2 <dmytro.makarov1@nure.ua>
2025-05-13 21:12:07 +0300

    update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,57 +1,58 @@
+// models/userModel.js
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema(
   {
-    name: {
-      type: String,
+    userId: {
+      type: Number,
       required: true,
+      unique: true
     },
     email: {
       type: String,
       required: true,
-      unique: true,
+      unique: true
     },
-    password: {
+    name: {
       type: String,
-      required: true,
+      required: true
     },
     phoneNumber: {
       type: String,
-      required: true,
+      required: true
     },
     profilePicture: {
-      type: String,
+      type: String
     },
     isBlocked: {
       type: Boolean,
-      default: false,
+      default: false
     },
     userType: {
       type: String,
       enum: ['Renter', 'PropertyOwner', 'Administrator', 'Moderator'],
-      default: 'Renter',
+      default: 'Renter'
     },
+    googleId: {
+      type: String,
+      sparse: true, // Дозволяє null, але унікальні значення
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationTokenExpires: {
+      type: Date,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     timestamps: true,
+    discriminatorKey: 'userType'
   }
 );
-
-// Хешування паролю перед збереженням
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    next();
-  }
-  
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
-
-// Метод порівняння паролів
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 const User = mongoose.model('User', userSchema);
 
