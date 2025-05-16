22cdccc82ab65a327d60e71d3f1d728d39eeb54c
APAXIC <andrii.vus@gmail.com>
2025-05-16 10:18:53 +0100

    changed back structure

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,11 @@
-// backend/server.js
-const express = require('express');
-const cors = require('cors');
-const passport = require('passport');
-const userRoutes = require('./server/routes/userRoutes');
-const authRoutes = require('./server/routes/authRoutes'); // якщо є
-require('dotenv').config();
+const app = require('./app');
+const connectDB = require('./config/connectDB');
+const dotenv = require('dotenv');
 
-const connectDB = require('./connectDB');
-
-const app = express();
-
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
-app.use('/api/auth', authRoutes);
-app.use('/api/user', userRoutes); // if implemented
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(passport.initialize());
-
-// Google strategy config
-require('./server/config/passport')(passport);
-
-// Routes
-
+dotenv.config();
 connectDB();
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server running on port ${process.env.PORT || 5000}`);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
