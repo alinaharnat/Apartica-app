3fd03ff95d503f695b57cab8418237cd1a137ad9
APAXIC <andrii.vus@gmail.com>
2025-06-06 23:19:17 +0100

    hosting

--- backend/controllers/propertyController.js
+++ backend/controllers/propertyController.js
@@ -216,7 +216,7 @@ const createPropertyWithRooms = asyncHandler(async (req, res) => {
     createdRooms.push(newRoom);
   }
 
-  const BASE_URL = process.env.BASE_URL || `${import.meta.env.VITE_API_URL}`;
+  const BASE_URL = process.env.BASE_URL || `${process.env.VITE_API_URL}`;
   const propertyPhotos = req.files.photos || [];
   const photosToSave = [];
   for (const photo of propertyPhotos) {
